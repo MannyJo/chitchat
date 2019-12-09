c7c293d45d3b170803f0240acdacbee9a35b141b
Manny Jo <jomansang@gmail.com>
2019-12-09 15:03:08 -0600

    Add server.js and connect to socket.io

--- server/server.js
+++ server/server.js
@@ -3,9 +3,12 @@ const http = require('http');
 const socketio = require('socket.io');
 
 const app = express();
-
 const server = http.createServer(app);
-
+const io = socketio(server);
 const PORT = process.env.PORT || 5000;
+
+io.on('connection', socket => {
+    socket.on('disconnect', () => console.log('User has left!'));
+});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
